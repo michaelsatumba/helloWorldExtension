d36f188dd09393f98ec1859ea6504ccb3fcdfc96
michaelsatumba <mjsatumba@gmail.com>
2023-12-20 14:48:19 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -12,3 +12,4 @@ document.addEventListener('DOMContentLoaded', function() {
 // 12/6/23
 // 12/7/23 deploying a tag - copy and paste images - control creating email to report - "ssi.frontend" format - security lecture cs50 understanding technology
 // 12/19/23 interviewed someone who didn't have projects and had an empty github repository
+// 12/20/23 a lot of css today using chat gpt, not much javascript stuff. html, flexbox, flex-basis. xsl conditional. found useful xml validation tool for debugging. debugging is awesome
