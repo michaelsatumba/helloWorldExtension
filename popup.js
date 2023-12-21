854a12c30d3a4fdab81d2d75d7a10aaa6dafd45f
michaelsatumba <mjsatumba@gmail.com>
2023-12-21 11:10:05 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -13,3 +13,4 @@ document.addEventListener('DOMContentLoaded', function() {
 // 12/7/23 deploying a tag - copy and paste images - control creating email to report - "ssi.frontend" format - security lecture cs50 understanding technology
 // 12/19/23 interviewed someone who didn't have projects and had an empty github repository
 // 12/20/23 a lot of css today using chat gpt, not much javascript stuff. html, flexbox, flex-basis. xsl conditional. found useful xml validation tool for debugging. debugging is awesome
+// 12/21/23 a lot of design tickets and css, chatgpt, no js yet. just flexbox, flex-basis. yeah. 2fa almost done. 
