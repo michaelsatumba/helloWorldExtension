1ef6a2169d0af0760b86996e25942516975052be
michaelsatumba <mjsatumba@gmail.com>
2024-01-19 08:57:41 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -30,4 +30,5 @@ document.addEventListener('DOMContentLoaded', function() {
  1/16/2024 working on creating new systems for us open. also helping with training like css and design stuff with jonathan. that's it for now.
  1/17/24 worked on nbpa updates, worked on tman summit, fdj login, and adjusted due dates for some tickets.
  1/18/24 main focus will be fdj today. ahh it got so busy so quickly. it's a good challenge though. small wins. work.
+ 1/19/24 super busy, but work. using css and html. width. interesting that the width attribute works but not the css width. focused on fdj edits and some versioning thing.
 */
