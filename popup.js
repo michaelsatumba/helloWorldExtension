2b55ef47d900b4cc3c73ffb0ca11b644bfd1c219
michaelsatumba <mjsatumba@gmail.com>
2024-01-23 09:13:34 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
  12/7/23 deploying a tag - copy and paste images - control creating email to report - "ssi.frontend" format - security lecture cs50 understanding technology
  12/19/23 interviewed someone who didn't have projects and had an empty github repository
  12/20/23 a lot of css today using chat gpt, not much javascript stuff. html, flexbox, flex-basis. xsl conditional. found useful xml validation tool for debugging. debugging is awesome
- 12/21/23 a lot of design tickets and css, chatgpt, no js yet. just flexbox, flex-basis. yeah. 2fa almost done. 
+ 12/21/23 a lot of design tickets and css, chatgpt, no js yet. just flexbox, flex-basis. yeah. 2fa almost done.
  12/26/23 looks like work is less busy. But there is still work. nbpa, allianz edits. just another day at work lol. work work work work work work work work work work work work work work. work work work work work work work work work work work work work work work.
  12/27/23 chill day. hanging. want to look at backlog tickets and start on the ticket for connor. gotta find how to change it lol.
  12/29/23 chillest day. i love debugging. going to sf. just keep swimming. do less.
@@ -32,4 +32,5 @@ document.addEventListener('DOMContentLoaded', function() {
  1/18/24 main focus will be fdj today. ahh it got so busy so quickly. it's a good challenge though. small wins. work.
  1/19/24 super busy, but work. using css and html. width. interesting that the width attribute works but not the css width. focused on fdj edits and some versioning thing.
  1/22/24 looks like it is going to be a busy day. need to ward off new requirements in ticket and create new ones. stick with the process! working on nbpa and tman summit today.
+ 1/23/24 looks like it is going to be a busy day too hahaha. allianz and nbpa. endeaveor. woo criticals. training jonathan. he's picking it up pretty fast.
 */
